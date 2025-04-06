53b549e95f8dedd35ba0e412c93840a0f1a7a0e6
sajedakhasawneh <sajedakhasawneh5@gmail.com>
2025-04-06 14:39:41 +0300

    by voice

--- Angular-Project-7/Angular-Project-7.Server/obj/Debug/net8.0/Angular-Project-7.Server.AssemblyInfo.cs
+++ Angular-Project-7/Angular-Project-7.Server/obj/Debug/net8.0/Angular-Project-7.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Angular-Project-7.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3adc72343412568af014ea0036feecf7208a8ae5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6655bf0f89332a1208dc60f9de21bf81a746f2da")]
 [assembly: System.Reflection.AssemblyProductAttribute("Angular-Project-7.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Angular-Project-7.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
